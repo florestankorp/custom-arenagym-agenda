8a82c61a34887feb66e1d864b3bc8b28f1cd5dbf
Florestan Korp <florestankorp@gmail.com>
2024-03-10 17:26:20 +0100

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: 'https://florestankorp.github.io/custom-arenagym-agenda/',
 	plugins: [svelte()]
 });
