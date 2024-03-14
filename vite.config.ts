9aebad787ebe8a156cd305bc8bad06615e11cab9
Florestan Korp <florestankorp@gmail.com>
2024-03-14 23:52:05 +0100

    chore: pwa config update

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: 'https://florestankorp.github.io/custom-arenagym-agenda/',
-	plugins: [svelte(), VitePWA({ registerType: 'autoUpdate' })],
+	plugins: [svelte(), VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' })],
 	test: {
 		globals: true,
 		environment: 'jsdom',
